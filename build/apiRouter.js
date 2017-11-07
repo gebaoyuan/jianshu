08c2dafd5cf8b7b2fa96355200d3d61b68686aec
gebaoyuan <yuansnowboy@163.com>
2017-11-07 09:26:33 +0800

    显示文章

--- build/apiRouter.js
+++ build/apiRouter.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var axios = require('axios');
 var router = express.Router();
+var cheerio = require('cheerio');
 
 router.get('/getArticleList', function (req, res, next) {
   axios.get('http://www.jianshu.com/mobile/trending/now', {
@@ -28,17 +29,45 @@ router.get('/getArticleList', function (req, res, next) {
 router.get('/p/:id', function (req, res, next) {
   axios.get('http://www.jianshu.com/p/' + req.params.id)
     .then(data => {
-      res.send({
-        code: 0,
-        msg: '成功',
-        data: data.data
-      })
+      console.log('成功');
+      var author = {}, article = {};
+      try {
+        var $body = cheerio.load(data.data)('body');
+        $author = $body.find('.author').eq(0),
+          $meta = $author.find('.meta');
+        author.avator = $author.find('.avatar img').attr('src');
+        author.id = ($author.find('.avatar').attr('href') || '').replace(/\/u\//, '');
+        author.nickname = $author.find('.name a').text();
+        // author.slug = '9d70440d2521';
+        // console.log($author.html());
+        article.contain = $body.find('.article').html();
+        article.publishTime = $meta.find('.publish-time').html();
+        article.wordsCount = ($meta.find('.wordage').text() || '').replace(/\D/g, '');
+        // article.viewsCount = ($meta.find('.views-count').text() || '').replace(/\D/g, '');
+        // article.commentsCount = ($meta.find('.comments-count').text() || '').replace(/\D/g, '');
+        // article.likesCount = ($meta.find('.likes-count').text() || '').replace(/\D/g, '');
+        res.send({
+          code: 0,
+          msg: '成功',
+          data:{
+            author:author,
+            article:article,
+          }
+        })
+      } catch (e) {
+        console.log('解析失败');
+        res.send({
+          code: 2,
+          msg: '获取文章详情失败',
+
+        })
+      }
     })
     .catch((e) => {
       console.log('失败');
       res.send({
         code: 1,
-        msg: '获取列表失败',
+        msg: '获取文章详情失败',
 
       })
     })
--- src/pages/articleDetail/index.vue
+++ src/pages/articleDetail/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-html="test"></div>
+    <div v-html="articleContainer" class="container note"></div>
   </div>
 </template>
 <script>
@@ -21,14 +21,19 @@
     },
     mounted() {
       this.$ajax.getArticleDetail(this.$route.params.id)
-        .then(res => {
-          console.log(res);
+        .then(({data, code, msg}) => {
+          if (!code) {
+            this.articleData = data;
+            this.articleContainer = data.article.contain
+          }
           this.test = res.data;
         })
     },
     data() {
       return {
-        test:'',
+        test: '',
+        articleData: '',
+        articleContainer: ''
       }
     },
     methods: {
@@ -44,5 +49,109 @@
 
 </style>
 <style lang="scss">
+  .container {
+    padding: 12px 18px 0;
+    a {
+      text-decoration: none;
+    }
+    p {
+      margin: 0 0 25px;
+    }
+    hr {
+      margin: 0 0 20px;
+      border-top: 1px solid #ddd;
+    }
+    img {
+      max-width: 100%;
+      height: auto;
+      vertical-align: middle;
+      border: 0;
+      cursor: -webkit-zoom-in;
+      transition: all .25s ease-in-out;
+    }
+    h3 {
+      font-size: 22px;
+    }
+    h1, h2, h3, h4, h5, h6 {
+      margin: 0 0 15px;
+      font-weight: 700;
+      color: #2f2f2f;
+      line-height: 1.7;
+      text-rendering: optimizelegibility;
+    }
+  }
+
+  .note {
+    .title {
+      margin: 15px 0;
+      font-size: 30px;
+      font-weight: 700;
+      font-family: -apple-system, SF UI Display, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif;
+      color: #2f2f2f;
+      word-break: break-word !important;
+      word-break: break-all;
+      line-height: 1.4;
+    }
+    .author {
+      margin-bottom: 20px;
+      font-size: 13px;
+      color: #b1b1b1;
+      text-align: left;
+      .avatar {
+
+        display: inline-block;
+        line-height: 1;
+        img {
+          width: 34px;
+          height: 34px;
+          border-radius: 17px;
+          margin-right: 10px;
+        }
+      }
+      .info {
+        display: inline-block;
+        .name {
+          margin-right: 3px;
+          font-size: 16px;
+          vertical-align: middle;
+          a {
+            margin: 0 5px;
+            font-size: 16px;
+            font-weight: 700;
+            color: #484848;
+            display: inline-block;
+            vertical-align: middle;
+
+          }
+        }
+        .meta {
+          margin-top: 5px;
+          font-size: 12px;
+          color: #969696;
+        }
+        img {
+          margin-top: -3px;
+          width: 20px;
+          height: 20px;
+          vertical-align: middle;
+        }
+      }
+    }
+    .show-content {
+      color: #2f2f2f;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 1.7;
+      blockquote {
+        padding: 20px;
+        margin-bottom: 25px;
+        background-color: #f7f7f7;
+        border-left: 6px solid #b4b4b4;
+        word-break: break-word!important;
+        word-break: break-all;
+        line-height: 30px;
+      }
+    }
+  }
 
 </style>
